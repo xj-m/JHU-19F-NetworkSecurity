b30f54b3dd8dad0303684583faf5e995acc82c81
马向君 <xiangjun@maxiangjuns-MacBook-Pro.local>
2019-09-09 13:01:05 -0400

    push

--- Exercise2/learn_python/output_foramt.py
+++ Exercise2/learn_python/output_foramt.py
@@ -2,11 +2,12 @@
 # print integer and float value 
 print("Geeks : % 2d, Portal : % 5.2f" %(1, 05.333))  
   
+# NOTE:  %[flags][width][.precision]type 
 # print integer value 
 print("Total students : % 1d, Boys : % 2d" %(240, 120)) 
   
 # print octal value 
 print("% 7.3o"% (25)) 
-  
+
 # print exponential value 
 print("% 10.3E"% (356.08977)) 
