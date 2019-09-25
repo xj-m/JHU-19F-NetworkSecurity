3f813d19fdd72910ab3d82567e3cd2aa4c722931
马向君 <xiangjun@maxiangjuns-MacBook-Pro.local>
2019-09-25 11:40:43 -0400

    x

--- netsec_env/Playground3/src/playground/network/devices/__init__.py
+++
@@ -1,5 +0,0 @@
-
-# now call these
-from .switch import Switch, UnreliableSwitch
-from .vnic import VNIC
-from .routing import HierarchyWAN
--- submisssions/netsec_env/bin/activate.fish
+++ submisssions/netsec_env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/student_20194/Xiangjun/2019-jhu-netsec/submisssions/HW5/netsec_env"
+set -gx VIRTUAL_ENV "/home/student_20194/Xiangjun/2019-jhu-netsec/submisssions/netsec_env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
