55cda21b14b6a3c92fba2294feef0e186e4a229a
xj-m <xj.ma@hotmail.com>
2019-10-01 11:48:41 -0400

    Arrange E7

--- submisssions/HW7/client_E7.py
+++ submisssions/HW7/client_E7.py
@@ -7,24 +7,19 @@ from cmdHandler_E7 import ClientCmdHandler, printx
 # from playground.common.logging import EnablePresetLogging, PRESET_DEBUG
 # EnablePresetLogging(PRESET_DEBUG)
 
+IPADDR = "20194.0.0.19000"
+PORT = 19007
 
 def getFirstPkt():
     pkt = AutogradeStartTest(
-        name="xiangjun", email="xjm@jhu.edu", team=2, port=1109)
+        name="xiangjun", email="xjm@jhu.edu", team=2, port=1107)
     with open("Packets_E7.py", "rb") as f:
         pkt.packet_file = f.read()
     return pkt
 
 
 def getCheckResPkt():
-    # TODO: set test id
     return AutogradeResultRequest(test_id="653feafb11e66d841e4647313bdcf4c701bc4b6a703b4a9ec62ff2b347ad0cce")
-
-
-# set the pkt that will send right after connection made
-Check_result_mode = False
-firstPkt = getCheckResPkt() if Check_result_mode else getFirstPkt()
-
 
 class ClientProtocol(asyncio.Protocol):
     def __init__(self, loop, firstPkt=None):
@@ -48,12 +43,16 @@ class ClientProtocol(asyncio.Protocol):
 
 def main(args):
     loop = asyncio.get_event_loop()
+    # NOTE: this for change into check mode 
+    if len(args) != 0:
+        firstPkt = getCheckResPkt() if args[0]=="check" else getFirstPkt()
+    else:
+        firstPkt = getFirstPkt()
     coro = playground.create_connection(lambda: ClientProtocol(loop=loop, firstPkt=firstPkt),
-                                        "20194.0.0.19000", 19007)  # for E5
+                                       IPADDR , PORT)  # for E5
     loop.run_until_complete(coro)
     loop.run_forever()
     loop.close()
-
 
 if __name__ == "__main__":
     main(sys.argv[1:])
